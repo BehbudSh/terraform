83bb6cde9f5c5b3915c9019ef9de4458b7b7ed5b
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-07-17 17:15:01 +0400

    Adding subnet

--- resources.tf
+++ resources.tf
@@ -19,6 +19,13 @@ resource "azurerm_virtual_network" "azy_virtual_network" {
      name = "subnet2"
      address_prefix = "10.0.2.0/24"
  }
+ # Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.azy_network.name}"
+    virtual_network_name = "${azurerm_virtual_network.azy_virtual_network.name}"
+    address_prefix       = "10.0.1.0/24"
+}
 }
 # Create public IPs
 resource "azurerm_public_ip" "myterraformpublicip" {
@@ -54,7 +61,7 @@ resource "azurerm_network_interface" "myterraformnic" {
 
     ip_configuration {
         name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.azy_virtual_network.id}"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
     }
@@ -62,7 +69,7 @@ resource "azurerm_network_interface" "myterraformnic" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+        resource_group = "${azurerm_resource_group.azy_network.name}"
     }
     
     byte_length = 8
