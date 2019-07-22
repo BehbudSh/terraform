c1ef27336e5779152156500b474a67b8b1d8633a
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-07-22 16:07:04 +0400

    vsphere provider

--- connections.tf
+++ connections.tf
@@ -5,6 +5,15 @@ provider "azurerm" {
   tenant_id = "${var.tenant_id}" 
 }
 
+provider "vsphere" {
+  user           = "administrator@vsphere.local"
+  password       = "Qwerty123!"
+  vsphere_server = "esxvi02.atl.lan"
+ 
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
